c640c6d431962a67245f6ccf314b5d5b2c6788c7
Tomas Tisocco <tomastisocco97@gmail.com>
2021-11-02 21:23:18 -0300

    Upgrade ShippingPage

--- lib/pages/shippingPage.dart
+++ lib/pages/shippingPage.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ShippingPage extends StatelessWidget {
 
   final int index;
-  late List<String> _producto = [''];
-  late List<String> _riceType = ['Blanco', 'Integral', 'Yamani', 'Bomba', 'Rojo', 'Salvaje'];
-  late List<String> _cosecha = [];
-  late List<String> _contrato = [];
-  late List<String> _partidaNumero = [];
-  late List<String> _pesoBruto = [];
-  late List<String> _pesoTara = [];
+  late List<String> _producto = ['23543','2341234','65433','3515123','43215','2413'];
+  late List<String> _riceType = ['Doble Carolina', 'Integral', 'Largo Fino', 'No se pasa', 'Aromatico', 'Yamani'];
+  late String _cosecha = '...';
+  late String _contrato = '...';
+  late String _partidaNumero = '...';
+  late List<int> _pesoBruto = [2500,4000,1345,4562,2000,3000];
+  late List<int> _pesoTara = [222,500,435,334,234,765];
   late List<String> _procedencia = ['Campo $index'];
 
   ShippingPage({Key? key, required this.index}) : super(key: key);
@@ -22,7 +23,22 @@ class ShippingPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Text(index.toString())
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Codigo de Producto: ${_producto[index]}'),
+              Text('Tipo de Arroz: ${_riceType[index]}'),
+              Text('Peso Bruto: ${_pesoBruto[index]}'),
+              Text('Peso Tara: ${_pesoTara[index]}'),
+              Text('Peso Neto: ${(_pesoBruto[index] - _pesoTara[index])}'),
+              Text('Procedencia: ${_procedencia[0]}'),
+              Text('Cosecha: $_cosecha'),
+              Text('Contrato: $_contrato'),
+              Text('Nuemro de Partida: $_partidaNumero'),
+              Text('Fecha de Envio: ${DateFormat.yMd().add_jm().format(DateTime.now().subtract(Duration(hours: index)))}'),
+            ]
+          ),
+        )
       ),
     );
   }
