f682609545d7a54dd71a7235dea8c9929c478217
Tomas Tisocco <tomastisocco97@gmail.com>
2021-12-22 17:29:49 -0300

    Config FireBase

--- lib/blocs/authBloc/authbloc_bloc.dart
+++ lib/blocs/authBloc/authbloc_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'authbloc_event.dart';
 part 'authbloc_state.dart';
@@ -7,7 +8,7 @@ part 'authbloc_state.dart';
 class AuthblocBloc extends Bloc<AuthblocEvent, AuthblocState> {
   AuthblocBloc() : super(AuthblocInitial()) {
     on<AuthblocEvent>((event, emit) {
-      // TODO: implement event handler
+      
     });
   }
 }
