ebd54a01c61fef64f6d7010b6ff9560523e7be55
Tomas Tisocco <tomastisocco97@gmail.com>
2021-12-30 20:46:20 -0300

    Implement Login Events

--- lib/blocs/login_bloc/login_event.dart
+++ lib/blocs/login_bloc/login_event.dart
@@ -6,3 +6,54 @@ abstract class LoginEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class EmailChanged extends LoginEvent  {
+  final String email;
+
+  EmailChanged({required this.email});
+
+  @override
+  List<Object> get props => [email];
+
+  @override
+  String toString() => 'EmailChanged: $email';
+}
+
+class PasswordChanged extends LoginEvent {
+  final String password;
+
+  PasswordChanged({required this.password});
+
+  @override
+  List<Object> get props => [password];
+
+  String toString() => 'PasswordChanged password: $password';
+}
+
+class Sumbitted extends LoginEvent {
+  final String email;
+  final String password;
+
+  Sumbitted({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() => 'Sumbbited: {email: $email, password: $password}';
+}
+
+class LoginWithGooglePressed extends LoginEvent {}
+
+class LoginWithCredentialPressed extends LoginEvent {
+  final String email;
+  final String password;
+
+  LoginWithCredentialPressed({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() => 'LoginWithCredential: {email: $email, password: $password}';
+}
