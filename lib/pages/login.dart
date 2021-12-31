96034a6c08100845c79594020129657ee6093513
Tomas Tisocco <tomastisocco97@gmail.com>
2021-12-31 10:32:21 -0300

    Start Login Page Implementation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,32 +1,25 @@
 import 'package:app_dos_hermanos/blocs/authentication_bloc/authenticaiton_bloc.dart';
+import 'package:app_dos_hermanos/blocs/login_bloc/login_bloc.dart';
 import 'package:app_dos_hermanos/classes/user.dart';
+import 'package:app_dos_hermanos/repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+import 'loginForm.dart';
 
-  static Route route(){
-    return MaterialPageRoute(builder: (_) => LoginPage());
-  }
+class LoginPage extends StatelessWidget {
+  final AuthenticationRepository authenticationRepository;
+
+  const LoginPage({Key? key, required this.authenticationRepository}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child:  Column(
-          children: <Widget>[
-            Text('LoginPage en Construccion'),
-            ElevatedButton(
-              child: Text('Registrado'),
-              onPressed: (){
-                BlocProvider.of<AuthenticationBloc>(context, listen: false).add(AuthenticationUserChanged(User(email: 'tomastisocco@gmali.com',id: '123451',name: 'TomasCandente',photo: 'ulrPhoto')));
-              },
-            )
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text('Login'), centerTitle: true),
+      body: BlocProvider<LoginBloc>(
+        create: (context) => LoginBloc(authenticationRepository: authenticationRepository),
+        child: LoginForm(authenticationRepository: authenticationRepository),
+      )
     );
   }
 }
---
+++ lib/pages/loginForm.dart
@@ -0,0 +1,139 @@
+import 'package:app_dos_hermanos/blocs/authentication_bloc/authenticaiton_bloc.dart';
+import 'package:app_dos_hermanos/blocs/login_bloc/login_bloc.dart';
+import 'package:app_dos_hermanos/classes/user.dart';
+import 'package:app_dos_hermanos/repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginForm extends StatefulWidget {
+  final AuthenticationRepository authenticationRepository;
+
+  const LoginForm({Key? key, required this.authenticationRepository}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  late LoginBloc _loginBloc;
+
+  AuthenticationRepository get _authenticationRepository => widget.authenticationRepository;
+
+  bool get isPopulated => _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isLoginButtonEnabled(LoginState state){
+    return state.isFormValid && isPopulated && !state.isSumbiting;
+  }
+
+  @override
+  void initState(){
+    super.initState ();
+    _loginBloc = BlocProvider.of<LoginBloc>(context);
+    _emailController.addListener(_onEmailChanged);
+    _passwordController.addListener(_onPasswordChanged);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener(
+      listener: (context, state){
+        if (state is FailLogin){
+          ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[Text('Login Failure'), Icon(Icons.error)],
+            ),
+            backgroundColor: Colors.red,
+          ));
+        }
+        if (state is Sumbitted){
+          ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[Text('Logging in...'), CircularProgressIndicator()],
+            ),
+          ));
+        }
+        if (state is SuccesLogin){
+          BlocProvider.of<AuthenticationBloc>(context).add(AuthenticationUserChanged(User(email: _emailController.text,id: '',photo: '',name: '')));
+        }
+      },
+      child: BlocBuilder<LoginBloc, LoginState>(
+        builder: (context, state){
+          return Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Image.asset(
+                      'assets/logo.png',
+                      height: 200,
+                    )
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.email),
+                      labelText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    validator: (_){
+                      return !state.isEmailValid? 'Invalid Email' : '';
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.password),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    validator: (_){
+                      return !state.isPasswordValid? 'Invalid Password' : '';
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          );
+        },
+      )
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onEmailChanged(){
+    _loginBloc.add(EmailChanged(email: _emailController.text));
+  }
+
+  void _onPasswordChanged(){
+    _loginBloc.add(PasswordChanged(password: _passwordController.text));
+  }
+}
