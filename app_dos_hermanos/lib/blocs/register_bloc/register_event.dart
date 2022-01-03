b7730607d6ebc971baeb6b6d8598d7aba73d50bf
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-03 12:23:15 -0300

    Implement Register Event

--- app_dos_hermanos/lib/blocs/register_bloc/register_event.dart
+++ app_dos_hermanos/lib/blocs/register_bloc/register_event.dart
@@ -1,4 +1,44 @@
 part of 'register_bloc.dart';
 
-@immutable
-abstract class RegisterEvent {}
+abstract class RegisterEvent extends Equatable {
+  const RegisterEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class EmailChanged extends RegisterEvent  {
+  final String email;
+
+  EmailChanged({required this.email});
+
+  @override
+  List<Object> get props => [email];
+
+  @override
+  String toString() => 'EmailChanged: $email';
+}
+
+class PasswordChanged extends RegisterEvent {
+  final String password;
+
+  PasswordChanged({required this.password});
+
+  @override
+  List<Object> get props => [password];
+
+  String toString() => 'PasswordChanged password: $password';
+}
+
+class Sumbitted extends RegisterEvent {
+  final String email;
+  final String password;
+
+  Sumbitted({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() => 'Sumbbited: {email: $email, password: $password}';
+}
