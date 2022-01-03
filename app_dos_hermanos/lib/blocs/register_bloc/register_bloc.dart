ddd133eaceccab8864ad72a968fb8d00e89b3664
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-03 12:31:52 -0300

    Implement Register Bloc

--- app_dos_hermanos/lib/blocs/register_bloc/register_bloc.dart
+++ app_dos_hermanos/lib/blocs/register_bloc/register_bloc.dart
@@ -1,14 +1,38 @@
+import 'package:app_dos_hermanos/classes/validators.dart';
+import 'package:app_dos_hermanos/repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(EmptyRegister()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
+  AuthenticationRepository authenticationRepository;
+
+  RegisterBloc({required this.authenticationRepository})
+  : super(EmptyRegister()){
+    on<EmailChanged>((event, emit) {
+      emit(new UpdateRegister(
+        isEmailValid: Validators.isValidEmail(event.email),
+        isPasswordValid: state.isPasswordValid
+      ));
+    });
+
+    on<PasswordChanged>((event,emit){
+      emit(new UpdateRegister(
+        isEmailValid: state.isEmailValid,
+        isPasswordValid: Validators.isValidPassword(event.password)
+      ));
+    });
+
+    on<Sumbitted>((event, state) async {
+      emit(new LoadingRegister());
+      try {
+        await authenticationRepository.signUp(email: event.email, password: event.password);
+        emit(SuccesRegister());
+      } catch (_) {
+        emit(FailRegister());
+      }
     });
   }
 }
