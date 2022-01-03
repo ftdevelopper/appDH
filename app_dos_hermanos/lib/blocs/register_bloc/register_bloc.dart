eb818b2d95793c9f6bbdf84fa83faadd9854290c
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-03 12:20:31 -0300

    Implement Register State

--- app_dos_hermanos/lib/blocs/register_bloc/register_bloc.dart
+++ app_dos_hermanos/lib/blocs/register_bloc/register_bloc.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial()) {
+  RegisterBloc() : super(EmptyRegister()) {
     on<RegisterEvent>((event, emit) {
       // TODO: implement event handler
     });
