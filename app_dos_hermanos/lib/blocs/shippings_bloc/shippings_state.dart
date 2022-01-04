779e6e089aac4704ae5213a5a502d7d538b3a17b
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-04 08:29:17 -0300

    implement Shipppings State

--- app_dos_hermanos/lib/blocs/shippings_bloc/shippings_state.dart
+++ app_dos_hermanos/lib/blocs/shippings_bloc/shippings_state.dart
@@ -1,6 +1,33 @@
 part of 'shippings_bloc.dart';
 
 @immutable
-abstract class ShippingsState {}
+abstract class ShippingsState extends Equatable {
+  const ShippingsState();
+
+  @override
+  List<Object?> get props => [];
+}
 
 class ShippingsInitial extends ShippingsState {}
+
+class ShippingsLoading extends ShippingsState {
+  @override
+  String toString() => 'Shippings Loading';
+}
+
+class ShippingsLoaded extends ShippingsState {
+  final List<Shipping> shippings;
+
+  const ShippingsLoaded({required this.shippings});
+
+  @override
+  List<Object?> get props => [shippings];
+
+  @override
+  String toString() => 'Shippings Loaded';
+}
+
+class ShippingsNotLoaded extends ShippingsState {
+  @override
+  String toString() => 'Shippings Not Loaded';
+}
