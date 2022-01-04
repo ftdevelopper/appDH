1ee00fc0a350a520a5a93bf0a7348711c8454e48
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-04 13:52:00 -0300

    Upgrade Shipping Repository for Post

--- app_dos_hermanos/lib/repository/shipping_repository.dart
+++ app_dos_hermanos/lib/repository/shipping_repository.dart
@@ -2,12 +2,41 @@ import 'package:app_dos_hermanos/classes/shipping.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ShippingRepository {
-  final databaseReference = FirebaseFirestore.instance;
+  final databaseReference = FirebaseFirestore.instance.collection("shippings");
 
   Stream<List<Shipping>> getSippings() {
-    return databaseReference.collection("shippings").snapshots()
+    return databaseReference.snapshots()
     .map((snapshot) {
       return snapshot.docs.map((doc) => Shipping.fromSnapShot(doc)).toList();
     });
+  }
+
+  Future<void> putShipping(Shipping shipping) async {
+    databaseReference.doc().set(
+      _mapShipping(shipping)
+    );
+  }
+
+  Map<String, String>_mapShipping(Shipping shipping){
+    Map<String, String> _shippingMap = {
+      'patent': shipping.patent,
+      'id': shipping.id,
+      'remiterFullWeight': shipping.remiterFullWeight ?? '',
+      'remiterFullWeightTime': shipping.remiterFullWeightTime ?? '',
+      'remiterFullWeightUser': shipping.remiterFullWeightUser ?? '',
+      'remiterLocation': shipping.remiterLocation ?? '',
+      'remiterTara': shipping.remiterTara ?? '',
+      'remiterTaraTime': shipping.remiterTaraTime ?? '',
+      'remiterTaraUser': shipping.remiterTaraUser ?? '',
+      'reciverFullWeight': shipping.reciverFullWeight ?? '',
+      'reciverFullWeightTime': shipping.reciverFullWeight ?? '',
+      'reciverFullWeightUser': shipping.reciverFullWeightUser ?? '',
+      'reciverLocation': shipping.reciverLocation ?? '',
+      'reciverTara': shipping.reciverTara ?? '',
+      'reciverTaraTime': shipping.reciverTaraTime ?? '',
+      'reciverTaraUser': shipping.reciverTaraUser ?? '',
+      'riceType': shipping.riceType ?? ''
+    };
+    return _shippingMap;
   }
 }
