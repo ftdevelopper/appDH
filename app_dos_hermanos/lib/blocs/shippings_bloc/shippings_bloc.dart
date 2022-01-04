b48bf7435f1832a0dd1925b332dde8f4a1eef8ef
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-04 08:37:19 -0300

    implement Shipppings Bloc

--- app_dos_hermanos/lib/blocs/shippings_bloc/shippings_bloc.dart
+++ app_dos_hermanos/lib/blocs/shippings_bloc/shippings_bloc.dart
@@ -1,13 +1,22 @@
+import 'package:app_dos_hermanos/classes/shipping.dart';
+import 'package:app_dos_hermanos/repository/shipping_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'shippings_event.dart';
 part 'shippings_state.dart';
 
 class ShippingsBloc extends Bloc<ShippingsEvent, ShippingsState> {
-  ShippingsBloc() : super(ShippingsInitial()) {
-    on<ShippingsEvent>((event, emit) {
-      // TODO: implement event handler
+  final ShippingRepository shippingRepository;
+  ShippingsBloc({required this.shippingRepository}) : super(ShippingsLoading()) {
+    on<LoadShippings>((event, emit) async {
+      try {
+        final List<Shipping> shippings = await shippingRepository.getSippings().first;
+        emit(ShippingsLoaded(shippings: shippings));
+      } catch (_) {
+        emit(ShippingsNotLoaded());
+      }
     });
   }
 }
