0baf543b4b8a2465dea4047e6401855a9485c5c7
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-10 09:51:14 -0300

    Implement DrawerEvents

--- app_dos_hermanos/lib/blocs/drawer_bloc/drawer_event.dart
+++ app_dos_hermanos/lib/blocs/drawer_bloc/drawer_event.dart
@@ -2,3 +2,9 @@ part of 'drawer_bloc.dart';
 
 @immutable
 abstract class DrawerEvent {}
+
+class LogoutPressed extends DrawerEvent{}
+
+class ChangeProfilePhoto extends DrawerEvent{}
+
+class ChangeLocation extends DrawerEvent{}
