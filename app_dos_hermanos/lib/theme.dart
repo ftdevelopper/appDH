7e018298ebca9e3e08468ab634fcebfee3b09cd2
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-18 08:47:02 -0300

    Change Theme

--- app_dos_hermanos/lib/theme.dart
+++ app_dos_hermanos/lib/theme.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 final theme = ThemeData(
-  primaryColorDark: const Color(0xFF0097A7),
-  primaryColorLight: const Color(0xFFB2EBF2),
-  primaryColor: const Color(0xFF00BCD4),
-  accentColor: const Color(0xFF009688),
+  primaryColor: const Color(0xFFec1b25),
+  accentColor: const Color(0xFF010100),
   scaffoldBackgroundColor: const Color(0xFFE0F2F1),
   inputDecorationTheme: InputDecorationTheme(
     border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))
