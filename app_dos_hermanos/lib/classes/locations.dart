de9b4dd50c2c385a7f8972625130f284e9ff5750
Tomas Tisocco <tomastisocco97@gmail.com>
2022-01-23 09:19:45 -0300

    Refactor Location class to handle local storage

--- app_dos_hermanos/lib/classes/locations.dart
+++ app_dos_hermanos/lib/classes/locations.dart
@@ -15,4 +15,16 @@ class Location {
   factory Location.fromName(String newname){
     return Location(name: newname);
   }
+
+  factory Location.fromJson(Map<String, dynamic> json){
+    return Location(
+      name: json["name"]
+    );
+  }
+
+  Map<String, String> toJson(){
+    return {
+      "name":name,
+    };
+  }
 }
