0e5b5387263300c784e98809446856b496f323c8
Tomas Tisocco <tomastisocco97@gmail.com>
2022-02-03 08:57:29 -0300

    Refactor Theme

--- app_dos_hermanos/lib/theme.dart
+++ app_dos_hermanos/lib/theme.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 final theme = ThemeData(
   primaryColor: const Color(0xFFec1b25),
-  accentColor: const Color(0xFF010100),
-  scaffoldBackgroundColor: const Color(0xFFE0F2F1),
+  colorScheme: ColorScheme(
+    background: const Color(0xFFE0F2F1),
+    brightness: Brightness.light,
+    error: Colors.red,
+    onBackground: const Color(0xFFE0F2F1),
+    onError: Colors.red,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.white,
+    primary: const Color(0xFFec1b25),
+    secondary: const Color(0xFFec1b25),
+    primaryVariant: Colors.white,
+    secondaryVariant: Colors.white,
+    surface: Colors.white,
+  ),
   inputDecorationTheme: InputDecorationTheme(
     border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))
   ),
