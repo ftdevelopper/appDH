2d9ed4737b305e2891ac67bf4f0635b6f3f0da79
Tomas Tisocco <tomastisocco97@gmail.com>
2022-02-28 17:28:11 -0300

    Create and implement In travel shipping widget

--- app_dos_hermanos/lib/pages/shippings/edit_shipping.dart
+++ app_dos_hermanos/lib/pages/shippings/edit_shipping.dart
@@ -1,3 +1,4 @@
+import 'package:app_dos_hermanos/pages/shippings/widgets/intravel_shipping_widget.dart';
 import 'package:app_dos_hermanos/pages/shippings/widgets/new_shipping_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -77,57 +78,7 @@ class _EditShippingState extends State<EditShipping> {
                       NewShippingWidget(),
                     if (_shipping.shippingState ==
                         ShippingStatus.inTravelShipping)
-                      Column(
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: InputDecoration(
-                                labelText: 'Humedad',
-                                border: InputBorder.none,
-                                icon: Icon(Icons.water_sharp)),
-                            controller: _humidityController,
-                            keyboardType: TextInputType.number,
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            validator: (_) {
-                              String humidity;
-                              _humidityController.text.length == 0
-                                  ? humidity = '1'
-                                  : humidity = _humidityController.text;
-                              return NewShippingValidator.isHumidityValid(
-                                  humidity);
-                            },
-                          ),
-                          Divider(),
-                          TextField(
-                            controller: TextEditingController()
-                              ..text = _shipping.remiterTara.toString(),
-                            enabled: false,
-                            decoration: InputDecoration(
-                                labelText: 'Peso Tara - Procedencia',
-                                icon: Icon(Icons.filter_1)),
-                          ),
-                          Divider(),
-                          TextField(
-                            controller: TextEditingController()
-                              ..text = _shipping.remiterFullWeight.toString(),
-                            enabled: false,
-                            decoration: InputDecoration(
-                                labelText: 'Peso Bruto - Procedencia',
-                                icon: Icon(Icons.filter_2)),
-                          ),
-                          Divider(),
-                          TextFormField(
-                            controller: TextEditingController(text: state.data),
-                            decoration: InputDecoration(
-                                labelText: 'Peso Bruto - Destino',
-                                icon: Icon(Icons.filter_3)),
-                            enabled: true,
-                            onChanged: (value){
-                              state.data = value;
-                            },
-                          ),
-                        ],
-                      ),
+                      IntravelShippingWidget(),
                     if (_shipping.shippingState ==
                         ShippingStatus.downloadedShipping)
                       Column(
